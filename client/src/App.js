e1dec494b52d18a784b86e92ba49afca53da3b64
Charles-Hébert Dalzon <charlby5@gmail.com>
2022-06-09 09:07:34 -0400

    Set up apolloc client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
 import Header from './components/Header';
+
+// set up apollo client
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className='container'>
-        <h1>Hello </h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className='container'>
+          <h1>Hello </h1>
+        </div>
+      </ApolloProvider>
     </>
   );
 }
