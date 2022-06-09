b8d3c5ec2fad56832f3a8d4a274d3551574a8bec
Charles-Hébert Dalzon <charlby5@gmail.com>
2022-06-08 20:58:52 -0400

    Setup graphiql middleware on the server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = require('./schema/schema');
 
 const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
 
 const port = process.env.PORT ?? 5000;
 
