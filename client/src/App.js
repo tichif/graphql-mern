eb0585b0af0fc1d03c5e9227d2c1790eb9aa7a6d
Charles-Hébert Dalzon <charlby5@gmail.com>
2022-06-10 20:56:18 -0400

    Create the 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -38,6 +39,7 @@ function App() {
           <div className='container'>
             <Routes>
               <Route path='/' element={<Home />} />
+              <Route path='*' element={<NotFound />} />
             </Routes>
           </div>
         </Router>
