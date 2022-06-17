4ac66613ab3685ce75eccca6577620b204c7ef76
Charles-Hébert Dalzon <charlby5@gmail.com>
2022-06-16 22:40:16 -0400

    Update project

---
+++ client/src/components/EditProjectForm.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+
+import { GET_PROJECT } from '../queries/projectQueries';
+import { UPDATE_PROJECT } from '../mutations/projectMutation';
+
+const EditProjectForm = ({ project }) => {
+  const [name, setName] = useState(project.name);
+  const [description, setDescription] = useState(project.description);
+  const [status, setStatus] = useState('');
+
+  const [updateProject] = useMutation(UPDATE_PROJECT, {
+    variables: { name, description, status, id: project.id },
+    refetchQueries: [{ query: GET_PROJECT, variables: { id: project.id } }],
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !status || !description) {
+      return alert('Please fill all the fields!!!');
+    }
+    updateProject(name, description, status);
+  };
+
+  return (
+    <div className='mt-5'>
+      <h3>Update Project Details</h3>
+      <form onSubmit={onSubmit}>
+        <div className='mb-3'>
+          <label className='form-label'>Name</label>
+          <input
+            type='text'
+            className='form-control'
+            id='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className='mb-3'>
+          <label className='form-label'>Description</label>
+          <textarea
+            className='form-control'
+            id='description'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className='mb-3'>
+          <label className='form-label'>Status</label>
+          <select
+            className='form-select'
+            id='status'
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            <option value='new'>Not Started</option>
+            <option value='progress'>In Progress</option>
+            <option value='completed'>Completed</option>
+          </select>
+        </div>
+        <button className='btn btn-primary' type='submit'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default EditProjectForm;
--- client/src/mutations/projectMutation.js
+++ client/src/mutations/projectMutation.js
@@ -27,6 +27,33 @@ export const ADD_PROJECT = gql`
   }
 `;
 
+export const UPDATE_PROJECT = gql`
+  mutation UpdateProject(
+    $id: ID!
+    $name: String!
+    $description: String!
+    $status: ProjectStatusUpdate!
+  ) {
+    updateProject(
+      id: $id
+      name: $name
+      description: $description
+      status: $status
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
 export const DELETE_PROJECT = gql`
   mutation DeleteProject($id: ID!) {
     deleteProject(id: $id) {
